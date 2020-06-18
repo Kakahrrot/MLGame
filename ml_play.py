aa082b46505407ac22b0d1845fde6fb81e8149b5
Kakahrrot <44106206+Kakahrrot@users.noreply.github.com>
2020-06-18 11:51:36 +0800

    Update ml_play.py

--- ml_play.py
+++ ml_play.py
@@ -9,7 +9,7 @@ def nearst(obj):
     else:
         tmp = obj[0]
         for car, X, y in obj[1:]:
-            if tmp[2] < y :
+            if tmp[2] > y :
                 tmp = [car, X, y]
         return tmp
 
@@ -29,11 +29,9 @@ class MLPlay:
         self.car_lane = self.car_pos[0] // 70       # lanes 0 ~ 8
         self.lanes = [35, 105, 175, 245, 315, 385, 455, 525, 595]  # lanes center
         self.line = 0
-        self.lines =   [70, 140, 210, 280, 350, 420, 490, 560]
+        self.lines =   [70, 140, 210, 280, 350, 420, 490, 560, 0]
         self.moveL = None
         self.moveR = None
-        self.nlcar = None
-        self.nrcar = None
         pass
 
     def update(self, scene_info):
@@ -48,7 +46,8 @@ class MLPlay:
                 self.car_lane = self.car_pos[0] // 70
                 self.line = round(self.car_pos[0] / 70) - 1
                 break
-
+        if len(self.car_pos) == 0:
+            return "SPEED"
         # for model training
         # if self.car_vel == 15:
         #     return ["SPEED", "MOVE_RIGHT"]
@@ -64,7 +63,7 @@ class MLPlay:
                 if ((y <= 200 and y >= 0 ) or (y <= 0 and y >= -80 )):
                     above.append([car, X, y])
                 # print("\t", X, car["pos"])
-                if (X  == self.line or X == self.line + 1) and ((y >= 0 and  y <= 200) or (y <= 0 and y >= -100 )):
+                if (X  == self.line or X == self.line + 1) and ((y >= 0 and  y <= 400) or (y <= 0 and y >= -100 )):
                 # if (X  == self.line or X == self.line + 1) and ((y >= 0) or (y <= 0 and y >= -100 )):
                 
                     # print("\t\t", car["pos"], X, "\n\n", flush = True)
@@ -93,6 +92,11 @@ class MLPlay:
                     offset = -10
                     right.append([car, X, y])
             if len(left) != 0 and len(right) != 0:
+                car, X, y = nearst(left+right)
+                if X == self.line:
+                    offset = 10
+                else:
+                    offset = -10
                 for lcar, lX, ly in left:
                     for rcar, rX, ry in right:
                         if abs(ly - ry) < 81:
@@ -112,11 +116,11 @@ class MLPlay:
         else:
             # print(self.player, "die", flush = True)
             # check for lane of self.line - 1 and self.line + 2
+            nlcar, nlX, nly = nearst(left)
+            nrcar, nrX, nry = nearst(right)
             if self.moveL == None and self.moveR == None:
                 self.moveR = True
                 self.moveL = True
-                self.nlcar, nlX, nly = nearst(left)
-                self.nrcar, nrX, nry = nearst(right)
                 for acar, aX, ay in above:
                     if aX == self.line - 1 and self.moveL:
                         if abs(ay - nly) < 81 or abs(ay) < 81:
@@ -128,21 +132,24 @@ class MLPlay:
                         break
             if self.moveL:
                 print(self.player, "move left", flush = True)
-                if self.car_pos[1] - self.nlcar["pos"][1] < 120:
+                if self.car_pos[1] - nlcar["pos"][1] < 120:
                     print("\tBREAK!!\n\n", flush = True)
                     return ["BRAKE", "MOVE_LEFT"]
                 else:
                     return ["SPEED", "MOVE_LEFT"]
             elif self.moveR:
                 print(self.player, "move right", flush = True)
-                if self.car_pos[1] - self.nrcar["pos"][1] < 120:
+                if self.car_pos[1] - nrcar["pos"][1] < 120:
                     print("\tBREAK!!\n\n", flush = True)
                     return ["BRAKE", "MOVE_RIGHT"]
                 else:
                     return ["SPEED", "MOVE_RIGHT"]
             else:
                 print(self.player, "die", flush = True)
-                return ["BRAKE"]
+                if nly < nry:
+                    return ["BRAKE", "MOVE_LEFT"]
+                else:
+                    return ["BRAKE", "MOVE_RIGHT"]
 
 
 
